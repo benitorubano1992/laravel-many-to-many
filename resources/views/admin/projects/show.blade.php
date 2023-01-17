3707b800ee24e09d4f78ceb4dea747bf589463b0
benitorubano1992 <rubanobenito@gmail.com>
2023-01-17 15:58:11 +0100

    first commit

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="container">
+        <h3 class="text-center my-3">{{ $project->title }}</h3>
+        <h5 class="text-center my-3 {{ $project->type ? 'text-primary' : 'd-none' }}">{{ $project->type?->name }}</h5>
+        <div class="d-flex justify-content-between my-2">
+            <div>{{ $project->created_at }}</div>
+            <div>{{ $project->slug }}</div>
+        </div>
+        @forelse ($tecnologies as $tecnology)
+            <span>#{{ $tecnology->name }}</span>
+        @empty
+            <p>Non ci sono tecnologie associate a questo progetto</p>
+        @endforelse
+
+        <div class="text-center my-3">
+            @if ($project->cover_image)
+                <img src="{{ asset('storage/' . $project->cover_image) }}" alt="immagine" class="w-50">
+            @else
+                <span>Not image yet</span>
+            @endif
+        </div>
+        <div>{{ $project->content }}</div>
+    </div>
+@endsection
